c2e826b64d27ccd5c076af3ba6d45b81c6c603c5
LeoMogiano <leomogiano@outlook.com>
2021-11-07 20:52:57 -0400

    AAÃumente un archivo JavaScript

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// VARIABLES
+
+// Let es para variables y const es para constantes
+
+let resultado;
+resultado = 3.9;
+//resultado = Math.round(resultado) //redondear
+//resultado = Math.ceil(resultado) // redondear hacia arriba
+resultado = Math.floor(resultado) //hacia abajo
+
+console.log(resultado);
